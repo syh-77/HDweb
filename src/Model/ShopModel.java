3723f93156379331b86985395291aa47379825af
moluyangyu <985286227@qq.com>
2023-07-16 14:54:36 +0800

    创建商店展示用商品的实体类，还有登录用的用户类的实体类，定义属性并且加上get，set方法，新建servlet界面，该界面是用于展示商品的

---
+++ src/Model/ShopModel.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class ShopModel {
+    public int pid;
+    public String showTxt;
+    public int number;
+    public int price;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getShowTxt() {
+        return showTxt;
+    }
+
+    public void setShowTxt(String showTxt) {
+        this.showTxt = showTxt;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/Model/UserModel.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class UserModel {
+    public  int userid;
+    public int passward;
+    public String  usernName;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getPassward() {
+        return passward;
+    }
+
+    public void setPassward(int passward) {
+        this.passward = passward;
+    }
+
+    public String getUsernName() {
+        return usernName;
+    }
+
+    public void setUsernName(String usernName) {
+        this.usernName = usernName;
+    }
+}
