762192d14f35cef4d2e63d4057b80d792cd857be
moluyangyu <985286227@qq.com>
2023-07-16 10:47:28 +0800

    导入包，新建servlet用于处理响应网页请求

---
+++ src/Servlet/learnServlet.java
@@ -0,0 +1,9 @@
+package Servlet;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/learn")
+public class learnServlet extends HttpServlet {
+}
